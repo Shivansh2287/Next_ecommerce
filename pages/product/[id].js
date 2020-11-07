461ce97238f643b4defea6d91a11f6cff238f156
Shivansh2287 <54775060+Shivansh2287@users.noreply.github.com>
2020-11-07 21:07:53 +0530

    cart

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -133,7 +133,7 @@ export async function getStaticProps({params:{id}}) {
 export async function getStaticPaths() {
     return {
       paths: [
-        { params: { id:"5f0f502b9cb9363990f3de6c" } } 
+        { params: { id:"5fa4571fcdedbd76491b300d" } } 
       ],
       fallback: true
   }
